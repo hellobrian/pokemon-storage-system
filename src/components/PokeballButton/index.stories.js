f8936a46ee2636bbf2fe5f9d3616569d33529403
Brian Han <brianhan87usa@gmail.com>
2018-08-02 22:45:51 -0500

    feat(storybook): setup knobs

--- src/components/PokeballButton/index.stories.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import PokeballButton from './index'
-
-storiesOf('PokeballButton', module).add('default', () => <PokeballButton />)
