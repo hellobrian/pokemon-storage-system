3f7038a0ade65f22367eac8380c1a3a9450d9e76
Brian Han <brianhan87usa@gmail.com>
2018-08-02 22:29:49 -0500

    feat: setup storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
---
+++ src/components/PokeballButton/index.stories.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import PokeballButton from './index'
+
+storiesOf('PokeballButton', module).add('default', () => <PokeballButton />)
